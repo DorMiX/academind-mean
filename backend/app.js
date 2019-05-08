520785199bc8c469e75f3e473da73b5c93b5cef1
DorMiX <dormany.robert@gmail.com>
2019-05-08 10:45:49 +0200

    Fetching Data from the DB

--- backend/app.js
+++ backend/app.js
@@ -59,24 +59,24 @@ app.post(
 app.get(
   "/api/posts",
   (req, res, next) => {
-    const posts = [
-      {
-        id: "aoa7ahuoq7zfaef",
-        title: "First server-side post",
-        content: "This is coming from server."
-      },
-      {
-        id: "9sifj9884fjwlfe",
-        title: "First server-side post",
-        content: "This is coming from server, too."
-      },
-    ];
-    res.status(200).json(
-      {
-        message: 'Post fetched successfully!',
-        posts: posts,
-      }
-    );
+    Post.find()
+      .then(
+        (documents) => {
+          res.status(200).json(
+            {
+              message: 'Post fetched successfully!',
+              posts: documents,
+            }
+          );
+        }
+      )
+      .catch(
+        (err) => {
+          console.log("Oppppsss! Error: " + err);
+        }
+      );
+
+
   }
 );
 
