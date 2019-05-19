bffe97d9d6b403352e11b924d50829ed327a6df0
DorMiX <dormany.robert@gmail.com>
2019-05-19 22:19:48 +0200

    Converting the Form from a Template Driven to a Reactive Approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import {
@@ -31,7 +31,7 @@ import { PostsService } from './posts/posts.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
